f2bb8b2d4c67091a06989fc07a1cab6a09662ae9
ThijsDeR <rech0004@hz.nl>
2022-03-31 11:17:25 +0200

    removed overflow

--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Game</title>
 </head>
-<body>
+<body style="overflow: hidden">
     <canvas id="game-canvas"></canvas>
 
     <script type="module" src="./js/main.js"></script>
