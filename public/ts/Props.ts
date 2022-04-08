8a4606ce81d44f801403b6b6620d59969edf7054
Jasonjason10 <ofil0001@hz.nl>
2022-04-08 13:54:07 +0200

    update props

--- public/ts/Props.ts
+++ public/ts/Props.ts
@@ -1,16 +1,21 @@
 export default class Props {
 private yPos: number
 private xPos: number
-private imageProp: string[];
-private xSpeed: number;
-private ySpeed: number
+private xVel: number
+private yVel: number
+private width: number
+private height:number
 
-public constructor(img:HTMLImageElement, xPos: number, yPos: number) {
-    this.imageProp = this.imageProp
-    this.xPos = xPos;
-    this.yPos = yPos;
-    this.ySpeed = 0;
-    this.xSpeed = 0
+
+
+public constructor() {
+    this.yVel = 0;
+    this.xVel = 0;
+
+  }
+
+  private getxPos(){
+    this.xPos
   }
 
 }
