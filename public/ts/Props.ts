303fd4f70aa93c0d40a7e08ef6ca82993054857e
Jasonjason10 <ofil0001@hz.nl>
2022-04-08 14:01:37 +0200

    Added gets to the prop class

--- public/ts/Props.ts
+++ public/ts/Props.ts
@@ -9,13 +9,38 @@ private height:number
 
 
 public constructor() {
+    this.xPos = this.xPos
+    this.yPos = this.yPos
     this.yVel = 0;
     this.xVel = 0;
+    this.width = this.width
+    this.height = this.height
 
   }
 
-  private getxPos(){
-    this.xPos
+  public getxPos() {
+    return this.xPos
   }
+
+  public getyPos() {
+    return this.yPos
+  }
+
+  public getyVel() {
+    return this.xVel
+  }
+
+  public getxVel() {
+    return this.xVel
+  }
+
+  public getWidth() {
+    return this.width
+  }
+
+  public getHeight() {
+    return this.height
+  }
+
 
 }
