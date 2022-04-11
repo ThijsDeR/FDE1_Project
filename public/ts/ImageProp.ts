fb4d390c7a01039bbbeee79c1b2360283505c661
Jasonjason10 <ofil0001@hz.nl>
2022-04-11 09:06:54 +0200

    added get to image prop

--- public/ts/ImageProp.ts
+++ public/ts/ImageProp.ts
@@ -1,5 +1,11 @@
 import Props from "./Props";
 
 export default class imageprop extends Props {
+    private image:HTMLImageElement
+
+
+    public getImage() {
+        return this.image
+    }
 
 }
