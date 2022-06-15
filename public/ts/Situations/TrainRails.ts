63476a6cbd47ed00d5a1f4c5b2dfcf8704c47848
PieterGeschiere <gesc0018@hz.nl>
2022-06-15 11:23:27 +0200

    added rails image

--- public/ts/Situations/TrainRails.ts
+++ public/ts/Situations/TrainRails.ts
@@ -8,12 +8,12 @@ import UserData from "../UserData.js";
 export default class TrainRails extends Situation {
     public constructor(canvas: HTMLCanvasElement, userData: UserData, stamina: number, upgrades: {stamina_resistance: {level: number, price: number}, stamina_gain: {level: number, price: number}}) {
         super(canvas, userData, upgrades)
-        this.background = new ImageProp(canvas.width / 3, -canvas.height, 0, 0, canvas.width / 2, canvas.height, './assets/img/objects/KruispuntZebraPad.png');
+        this.background = new ImageProp(canvas.width / 3, -canvas.height, 0, 0, canvas.width / 2, canvas.height, './assets/img/TreinSpoor.png');
         this.props = [
-            new Frikandelbroodje(this.background.getXPos() + (this.background.getWidth() / 1.7), this.background.getYPos() + (this.background.getHeight() / 8), 0, 0, this.background.getWidth() / 16, this.background.getHeight() / 9, './assets/img/objects/frikandelbroodje.png', 10)
+            new Frikandelbroodje(this.background.getXPos() + (this.background.getWidth() / 1.6), this.background.getYPos() + (this.background.getHeight() / 5), 0, 0, this.background.getWidth() / 16, this.background.getHeight() / 9, './assets/img/objects/frikandelbroodje.png', 10)
         ]
 
-        const train = new ImageProp(this.background.getXPos() - (this.background.getWidth() / 10), this.background.getYPos() - (this.background.getHeight() / 3) , 0.3, 0, this.background.getWidth() / 16, this.background.getHeight() , './assets/img/players/fiets1.png')
+        const train = new ImageProp(this.background.getXPos() - (this.background.getWidth() / 10), this.background.getYPos() - (this.background.getHeight() / 4) , 0.3, 0, this.background.getWidth() / 16, this.background.getHeight() , './assets/img/players/fiets1.png')
     
         if (Game.randomInteger(0, 1) === 1) {
             this.props.push(train)
