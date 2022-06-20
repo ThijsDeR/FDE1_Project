e3e7dbda9b59c2695723ecdd472ab18745779256
Jay-Groen <groe0076@hz.nl>
2022-06-20 11:19:00 +0200

    uitleg klaar

--- resources/views/explanation.blade.php
+++ resources/views/explanation.blade.php
@@ -6,10 +6,17 @@
 @endsection
 
 @section('content')
-<div class="goal">
-    <h1>Het doel van het spel is dat je zo ver mogelijk komt in het verkeer</h1>
-    <p>Je gaat sneller met de top arrow key
-       en remt de de spatie key
-    </p>
+<div class="text-container">
+    <h1>Uitleg</h1>
+  <ul>
+    <li>Het doel van het spel is dat je zo ver mogelijk komt in het verkeer</li>
+    <li>Onderweg kom je meerdere verkeersituaties tegen, die je veilig moet voltooien</li>
+    <li>Je moet een zo hoog mogelijk punten te krijgen om upgrades en skins te kopen</li>
+</ul>
+<div class="controls" style="margin-top: 4vh;">
+    <h3>Je bestuurt je fiets met de arrow keys en remt met de spatiebalk </h3>
+</div>
+<img style="margin-top: 4vh;" src="./assets/img/objects/frikandelbroodje.png">
+<h3 style="margin-top: 0vh;">Probeer de frikandelbroodjes te verzamelen voor meer stamina</h3>
 </div>
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,13 +9,12 @@
     <div class="big-button-container">
         @if (auth()->user())
           <a href="./game" id="nice-button" class="nice-button" role="button">Start</a>
+          <a href="./explanation" id="nice-button" class="nice-button" role="button">Explanation</a>
         @else
           <a href="{{route('loginView')}}" id="nice-button" class="nice-button" role="button">login</a>
         @endif
-        <a href="./explanation" id="nice-button" class="nice-button" role="button">Explanation</a>
+
+
     </div>
-    <!-- <div class="big-button-container">
-        <a href="./explanation" id="nice-button" class="nice-button" role="button">Explanation</a>
-    </div> -->
     <div class="mute-button active"></div>
 @endsection
