9c8364f6b1c7191446a2c8dc3b3c96455be26946
ThijsDeR <rech0004@hz.nl>
2022-06-23 10:40:24 +0200

    fix

--- resources/views/auth/terms_of_service.blade.php
+++ resources/views/auth/terms_of_service.blade.php
@@ -13,16 +13,16 @@
   <ul>
     <li>Lees deze tekst nauwkeurig.</li>
     <li>Als je een account registreert ga je akkoord met de voorwaarden.</li>
-    <li>Wij kunnen jou account verwijderen als je een offensieve naam hebt gekozen waardoor je al je voortgang kwijt raakt.</li>
+    <li>Wij kunnen jouw account verwijderen als je een offensieve naam hebt gekozen waardoor je al je voortgang kwijt raakt.</li>
     <li>Wij stellen voor om een uniek wachtwoord te gebruiken voor ons spel en dus niet een wachtwoord die je voor andere zaken ook gebruikt,</li>
     <li>want hackers en scammers kunnen bij deze gegevens komen</li>
 </ul>
     <h1>Contact gegevens</h1>
    <ul>
     <li>Als er iets is dat je wilt melden over de game (zoals bugs, suggesties) stuur een mail naar:</li>
-    <li>teagay@gmail.com</li>
+    <li>rech0004@hz.nl</li>
     <li>of</li>
-    <li>teacringe@gmail.com</li>
+    <li>thijsderechter@gmail.com</li>
 
 </ul>
 </div>
