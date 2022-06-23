9adb8548102aff073682c43dbc74a4b4a30f60f5
Jay-Groen <groe0076@hz.nl>
2022-06-23 10:41:37 +0200

    tractor

--- public/js/Props/Tractor.js
+++ public/js/Props/Tractor.js
@@ -1,6 +1,6 @@
 import ImageProp from "./ImageProp.js";
 export class Tractor extends ImageProp {
     constructor(xPos, yPos, xVel, yVel, width, height) {
-        super(xPos, yPos, xVel, yVel, width, height, './assets/img/objects/car.png');
+        super(xPos, yPos, xVel, yVel, width, height, './assets/img/objects/tractor_1.png');
     }
 }
