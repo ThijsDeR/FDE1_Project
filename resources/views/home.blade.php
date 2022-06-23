030baead2f3fb62d2453529b0d1536c269470709
Jay-Groen <groe0076@hz.nl>
2022-06-23 09:50:16 +0200

    engels naar nederlands

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,7 @@
     <div class="big-button-container">
         @if (auth()->user())
           <a href="./game" id="nice-button" class="nice-button" role="button">Start</a>
-          <a href="./explanation" id="nice-button" class="nice-button" role="button">Explanation</a>
+          <a href="./explanation" id="nice-button" class="nice-button" role="button">Uitleg</a>
         @else
           <a href="{{route('loginView')}}" id="nice-button" class="nice-button" role="button" style="top: 40vh;">login</a>
         @endif
