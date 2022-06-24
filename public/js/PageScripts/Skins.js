66508e677761a3794122a9836bfde7bc9021bd33
ThijsDeR <rech0004@hz.nl>
2022-06-24 11:59:22 +0200

    fixed skin stuff and removed all bicycles

--- public/js/PageScripts/Skins.js
+++ public/js/PageScripts/Skins.js
@@ -10,12 +10,15 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 import UserData from "../UserData.js";
 const skins = document.querySelectorAll('.skin');
 const userdata = new UserData();
+const vp = document.querySelector('span#vp');
 skins.forEach((skin) => __awaiter(void 0, void 0, void 0, function* () {
     const type = skin.getAttribute('type');
     const buyBtn = skin.querySelector('button.buy');
     skin.querySelector('button').addEventListener('click', () => __awaiter(void 0, void 0, void 0, function* () {
         const skin_data = yield userdata.buySkin(type, skin.id);
         if (skin_data.status === 200) {
+            const playerData = yield userdata.getPlayerData();
+            vp.innerHTML = playerData.vp;
             // worked
             skin.remove();
         }
--- public/ts/PageScripts/Skins.ts
+++ public/ts/PageScripts/Skins.ts
@@ -3,7 +3,7 @@ import UserData from "../UserData.js"
 const skins = document.querySelectorAll('.skin')
 
 const userdata = new UserData();
-
+const vp = document.querySelector('span#vp')!;
 
 skins.forEach(async (skin) => {
     const type = skin.getAttribute('type')!;
@@ -11,6 +11,8 @@ skins.forEach(async (skin) => {
     skin.querySelector('button')!.addEventListener('click', async () => {
         const skin_data = await userdata.buySkin(type, skin.id)
         if (skin_data.status === 200) {
+            const playerData = await userdata.getPlayerData()
+            vp.innerHTML = playerData.vp
             // worked
             skin.remove()
         }
