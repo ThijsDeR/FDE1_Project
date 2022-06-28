41c9a29cbac7319d94d60018d33e13e6bb38cf80
ThijsDeR <rech0004@hz.nl>
2022-06-28 10:48:23 +0200

    terms fix

--- resources/views/auth/terms_of_service.blade.php
+++ resources/views/auth/terms_of_service.blade.php
@@ -1,8 +1,7 @@
 @extends('common.master')
 
 @section('head-content')
-  <script src="{{ asset('js/PageScripts/Mainscreen.js') }}" defer></script>
-  <link href="{{ asset('css/menuStyles.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/explanationStyles.css') }}" rel="stylesheet">
 @endsection
 
 @section('content')
